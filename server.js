b89b17d6461e70ff3d03c5c6c1ecbc465b1adf11
unknown <maxim00066@gmail.com>
2021-07-07 19:26:10 +0300

    refact: added middleware parcel

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const Bundler = require('parcel-bundler');
+const bundler = new Bundler('src/pages/*.pug');
 
-app.use(express.static('dist'));
+app.use(bundler.middleware());
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
